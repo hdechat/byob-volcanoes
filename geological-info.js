96d875caba370d73b67a11878f8ea5723a2ab9a3
Helen Dechat <hdechat@gmail.com>
2018-07-10 16:06:36 -0600

    Add data files

---
+++ geological-info.js
@@ -0,0 +1,147 @@
+export default JSON.parse([
+  {
+    "volcano_type": "Maar(s)",
+    "rock_type": "Foidite",
+    "tectonic": "Rift zone"
+  },
+  {
+    "volcano_type": "Lava dome(s)",
+    "rock_type": "Basalt",
+    "tectonic": "Rift zone"
+  },
+  {
+    "volcano_type": "Pyroclastic cone(s)",
+    "rock_type": "Trachybasalt",
+    "tectonic": "Intraplate"
+  },
+  {
+    "volcano_type": "Pyroclastic cone(s)",
+    "rock_type": "Basalt",
+    "tectonic": "Intraplate"
+  },
+  {
+    "volcano_type": "Explosion crater(s)",
+    "rock_type": null,
+    "tectonic": "Subduction zone"
+  },
+  {
+    "volcano_type": "Caldera",
+    "rock_type": "Trachyte",
+    "tectonic": "Subduction zone"
+  },
+  {
+    "volcano_type": "Caldera",
+    "rock_type": "Foidite",
+    "tectonic": "Subduction zone"
+  },
+  {
+    "volcano_type": "Caldera",
+    "rock_type": "Trachyte",
+    "tectonic": "Subduction zone"
+  },
+  {
+    "volcano_type": "Stratovolcano",
+    "rock_type": "Phono-tephrite",
+    "tectonic": "Subduction zone"
+  },
+  {
+    "volcano_type": "Complex",
+    "rock_type": "Trachyte",
+    "tectonic": "Subduction zone"
+  },
+  {
+    "volcano_type": "Submarine",
+    "rock_type": "Phonolite",
+    "tectonic": "Subduction zone"
+  },
+  {
+    "volcano_type": "Stratovolcano",
+    "rock_type": "Trachyandesite",
+    "tectonic": "Subduction zone"
+  },
+  {
+    "volcano_type": "Stratovolcano",
+    "rock_type": "Andesite",
+    "tectonic": "Subduction zone"
+  },
+  {
+    "volcano_type": "Stratovolcano(es)",
+    "rock_type": "Rhyolite",
+    "tectonic": "Subduction zone"
+  },
+  {
+    "volcano_type": "Stratovolcano(es)",
+    "rock_type": "Trachybasalt",
+    "tectonic": "Subduction zone"
+  },
+  {
+    "volcano_type": "Stratovolcano(es)",
+    "rock_type": "Trachybasalt",
+    "tectonic": "Subduction zone"
+  },
+  {
+    "volcano_type": "Submarine",
+    "rock_type": "Trachybasalt",
+    "tectonic": "Rift zone"
+  },
+  {
+    "volcano_type": "Shield",
+    "rock_type": "Rhyolite",
+    "tectonic": "Rift zone"
+  },
+  {
+    "volcano_type": "Submarine",
+    "rock_type": null,
+    "tectonic": null
+  },
+  {
+    "volcano_type": "Lava dome(s)",
+    "rock_type": "Andesite",
+    "tectonic": "Subduction zone"
+  },
+  {
+    "volcano_type": "Stratovolcano(es)",
+    "rock_type": "Rhyolite",
+    "tectonic": "Subduction zone"
+  },
+  {
+    "volcano_type": "Shield(s)",
+    "rock_type": "Dacite",
+    "tectonic": "Subduction zone"
+  },
+  {
+    "volcano_type": "Stratovolcano",
+    "rock_type": "Dacite",
+    "tectonic": "Subduction zone"
+  },
+  {
+    "volcano_type": "Lava dome(s)",
+    "rock_type": "Rhyolite",
+    "tectonic": "Subduction zone"
+  },
+  {
+    "volcano_type": "Pyroclastic cone(s)",
+    "rock_type": "Trachybasalt",
+    "tectonic": "Subduction zone"
+  },
+  {
+    "volcano_type": "Pyroclastic cone(s)",
+    "rock_type": "Trachyandesite",
+    "tectonic": "Intraplate"
+  },
+  {
+    "volcano_type": "Stratovolcano",
+    "rock_type": "Dacite",
+    "tectonic": "Intraplate"
+  },
+  {
+    "volcano_type": "Lava dome",
+    "rock_type": "Rhyolite",
+    "tectonic": "Intraplate"
+  },
+  {
+    "volcano_type": "Lava dome",
+    "rock_type": "Rhyolite",
+    "tectonic": "Intraplate"
+  }
+])

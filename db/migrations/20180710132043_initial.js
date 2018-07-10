9d94417f27b346bc296fef243f5e53bdbd54f4ba
Helen Dechat <hdechat@gmail.com>
2018-07-10 13:42:54 -0600

    Add knex file and migrations

---
+++ db/migrations/20180710132043_initial.js
@@ -0,0 +1,32 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('geological_info', table => {
+      table.increments('id').primary();
+      table.string('volcano_type');
+      table.string('rock_type');
+      table.string('tectonic');
+
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('volcanoes', table => {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('country');
+      table.integer('last_eruption');
+      table.integer('geological_info_id').unsigned()
+      table.foreign('geological_info_id')
+        .references('geological_info.id');
+
+      table.timestamps(true, true);
+    });
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('volcanoes'),
+    knex.schema.dropTable('geological_info')
+  ]);
+};

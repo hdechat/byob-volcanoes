4ea3670449302b2a408b3f05e07ee13ec3d19eaa
meloncatty <k.handel66@gmail.com>
2018-07-10 19:56:39 -0600

    Change exports

--- data/volcanoes-data.js
+++ data/volcanoes-data.js
@@ -1,147 +1,145 @@
-export default JSON.parse([
-  {
-    "name": "West Eifel Volcanic Field",
-    "country": "Germany",
-    "last_known_eruption": "8300 BCE"
-  },
-  {
-    "name": "Chaine des Puys",
-    "country": "France",
-    "last_known_eruption": "4040 BCE"
-  },
-  {
-    "name": "Olot Volcanic Field",
-    "country": "Spain",
-    "last_known_eruption": "Unknown"
-  },
-  {
-    "name": "Calatrava Volcanic Field",
-    "country": "Spain",
-    "last_known_eruption": "3600 BCE"
-  },
-  {
-    "name": "Larderello",
-    "country": "Italy",
-    "last_known_eruption": "1282 CE"
-  },
-  {
-    "name": "Vulsini",
-    "country": "Italy",
-    "last_known_eruption": "104 BCE"
-  },
-  {
-    "name": "Colli Albani",
-    "country": "Italy",
-    "last_known_eruption": "Unknown"
-  },
-  {
-    "name": "Campi Flegrei",
-    "country": "Italy",
-    "last_known_eruption": "1538 CE"
-  },
-  {
-    "name": "Vesuvius",
-    "country": "Italy",
-    "last_known_eruption": "1944 CE"
-  },
-  {
-    "name": "Ischia",
-    "country": "Italy",
-    "last_known_eruption": "1302 CE"
-  },
-  {
-    "name": "Palinuro",
-    "country": "Italy",
-    "last_known_eruption": "8040 BCE"
-  },
-  {
-    "name": "Stromboli",
-    "country": "Italy",
-    "last_known_eruption": "2018 CE"
-  },
-  {
-    "name": "Panarea",
-    "country": "Italy",
-    "last_known_eruption": "Unknown"
-  },
-  {
-    "name": "Lipari",
-    "country": "Italy",
-    "last_known_eruption": "1230 CE"
-  },
-  {
-    "name": "Vulcano",
-    "country": "Italy",
-    "last_known_eruption": "1890 CE"
-  },
-  {
-    "name": "Etna",
-    "country": "Italy",
-    "last_known_eruption": "2018 CE"
-  },
-  {
-    "name": "Campi Flegrei Mar Sicilia",
-    "country": "Italy",
-    "last_known_eruption": "1867 CE"
-  },
-  {
-    "name": "Pantelleria",
-    "country": "Italy",
-    "last_known_eruption": "1891 CE"
-  },
-  {
-    "name": "Marsili",
-    "country": "Italy",
-    "last_known_eruption": "1050 BCE"
-  },
-  {
-    "name": "Methana",
-    "country": "Greece",
-    "last_known_eruption": "258 BCE"
-  },
-  {
-    "name": "Milos",
-    "country": "Greece",
-    "last_known_eruption": "140 CE"
-  },
-  {
-    "name": "Santorini",
-    "country": "Greece",
-    "last_known_eruption": "1950 CE"
-  },
-  {
-    "name": "Nisyros",
-    "country": "Greece",
-    "last_known_eruption": "1888 CE"
-  },
-  {
-    "name": "Yali",
-    "country": "Greece",
-    "last_known_eruption": "Unknown"
-  },
-  {
-    "name": "Kula",
-    "country": "Turkey",
-    "last_known_eruption": "Unknown"
-  },
-  {
-    "name": "Karapinar Field",
-    "country": "Turkey",
-    "last_known_eruption": "Unknown"
-  },
-  {
-    "name": "Hasan Dagi",
-    "country": "Turkey",
-    "last_known_eruption": "Unknown"
-  },
-  {
-    "name": "Gollu Dag",
-    "country": "Turkey",
-    "last_known_eruption": "Unknown"
-  },
-  {
-    "name": "Yali",
-    "country": "Greece",
-    "last_known_eruption": "Unknown"
-  }
-])
+module.exports = [{
+  name: 'West Eifel Volcanic Field',
+  country: 'Germany',
+  last_known_eruption: '8300 BCE'
+},
+{
+  name: 'Chaine des Puys',
+  country: 'France',
+  last_known_eruption: '4040 BCE'
+},
+{
+  name: 'Olot Volcanic Field',
+  country: 'Spain',
+  last_known_eruption: 'Unknown'
+},
+{
+  name: 'Calatrava Volcanic Field',
+  country: 'Spain',
+  last_known_eruption: '3600 BCE'
+},
+{
+  name: 'Larderello',
+  country: 'Italy',
+  last_known_eruption: '1282 CE'
+},
+{
+  name: 'Vulsini',
+  country: 'Italy',
+  last_known_eruption: '104 BCE'
+},
+{
+  name: 'Colli Albani',
+  country: 'Italy',
+  last_known_eruption: 'Unknown'
+},
+{
+  name: 'Campi Flegrei',
+  country: 'Italy',
+  last_known_eruption: '1538 CE'
+},
+{
+  name: 'Vesuvius',
+  country: 'Italy',
+  last_known_eruption: '1944 CE'
+},
+{
+  name: 'Ischia',
+  country: 'Italy',
+  last_known_eruption: '1302 CE'
+},
+{
+  name: 'Palinuro',
+  country: 'Italy',
+  last_known_eruption: '8040 BCE'
+},
+{
+  name: 'Stromboli',
+  country: 'Italy',
+  last_known_eruption: '2018 CE'
+},
+{
+  name: 'Panarea',
+  country: 'Italy',
+  last_known_eruption: 'Unknown'
+},
+{
+  name: 'Lipari',
+  country: 'Italy',
+  last_known_eruption: '1230 CE'
+},
+{
+  name: 'Vulcano',
+  country: 'Italy',
+  last_known_eruption: '1890 CE'
+},
+{
+  name: 'Etna',
+  country: 'Italy',
+  last_known_eruption: '2018 CE'
+},
+{
+  name: 'Campi Flegrei Mar Sicilia',
+  country: 'Italy',
+  last_known_eruption: '1867 CE'
+},
+{
+  name: 'Pantelleria',
+  country: 'Italy',
+  last_known_eruption: '1891 CE'
+},
+{
+  name: 'Marsili',
+  country: 'Italy',
+  last_known_eruption: '1050 BCE'
+},
+{
+  name: 'Methana',
+  country: 'Greece',
+  last_known_eruption: '258 BCE'
+},
+{
+  name: 'Milos',
+  country: 'Greece',
+  last_known_eruption: '140 CE'
+},
+{
+  name: 'Santorini',
+  country: 'Greece',
+  last_known_eruption: '1950 CE'
+},
+{
+  name: 'Nisyros',
+  country: 'Greece',
+  last_known_eruption: '1888 CE'
+},
+{
+  name: 'Yali',
+  country: 'Greece',
+  last_known_eruption: 'Unknown'
+},
+{
+  name: 'Kula',
+  country: 'Turkey',
+  last_known_eruption: 'Unknown'
+},
+{
+  name: 'Karapinar Field',
+  country: 'Turkey',
+  last_known_eruption: 'Unknown'
+},
+{
+  name: 'Hasan Dagi',
+  country: 'Turkey',
+  last_known_eruption: 'Unknown'
+},
+{
+  name: 'Gollu Dag',
+  country: 'Turkey',
+  last_known_eruption: 'Unknown'
+},
+{
+  name: 'Yali',
+  country: 'Greece',
+  last_known_eruption: 'Unknown'
+}]

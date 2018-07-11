3d3cab69830f579be890bd16ef001c3e2231c75e
Krista Handel <k.handel66@gmail.com>
2018-07-11 13:27:34 -0600

    Add GET requests

--- server.js
+++ server.js
@@ -9,6 +9,26 @@ const database = require('knex')(configuration);
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || 3000);
 
+app.get('/api/v1/volcanoes', (request, response) => {
+  database('volcanoes').select()
+    .then(volcanoes => {
+      response.status(200).json({ volcanoes })
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+});
+
+app.get('/api/v1/geo-info', (request, response) => {
+  database('geological_info').select()
+    .then(geoInfo => {
+      response.status(200).json({ geoInfo })
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+});
+
 app.listen(app.get('port'), () => {
   console.log(`Sever is running on ${app.get('port')}.`)
 });
